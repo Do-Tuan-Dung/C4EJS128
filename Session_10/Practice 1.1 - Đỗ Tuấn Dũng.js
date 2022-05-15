5990e45a1911b789fec5f25477d7eb55893ced4e
Đỗ Tuấn Dũng <madeoutoffire.dotuandung@gmail.com>
2022-05-15 19:27:53 +0700

    10.1

---
+++ Session_10/Practice 1.1 - Đỗ Tuấn Dũng.js
@@ -0,0 +1,50 @@
+//Bài 1:
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function migratoryBirds(arr) {
+    let sighted = arr[0];
+    let mostSighted = 0;
+    for (i = 1; i < sighted.length; i++) {
+        if (arr[i] > sighted) {
+            mostSighted = i;
+            sighted = arr[i]
+        }
+    }
+    return mostSighted;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
